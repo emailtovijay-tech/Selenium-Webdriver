178c794b4d7bc19e7d02e702c7c54d83db334d54
emailtovijay-tech <emailtovijay050390@gmail.com>
2025-10-15 11:28:39 +0530

    Adding new Drag and drop TC and updating the Scroll file.

---
+++ Selenium-Webdriver/HandlingDragandDrop.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi.Script;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Xml.Linq;
+
+
+namespace Selenium_Webdriver
+{
+    internal class HandlingDragandDrop
+    {
+        [Test]
+        public void HandleDragandDrop()
+        {
+            // Launching the Browser
+            IWebDriver driver = new ChromeDriver();
+
+            // Global implicit wait 10 seconds
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("https://testautomationpractice.blogspot.com/");
+            driver.Manage().Window.Maximize();
+
+
+            // Scroll down by 1500 pixels
+            IJavaScriptExecutor JS = (IJavaScriptExecutor)driver;
+            JS.ExecuteScript("window.scrollBy(0, 1500);");
+          
+
+            IWebElement Source = driver.FindElement(By.XPath("//p[normalize-space()='Drag me to my target']"));
+            IWebElement Target = driver.FindElement(By.XPath("//div[@id='droppable']"));
+
+            Thread.Sleep(3000);
+            Actions DD = new Actions(driver);
+
+            // Draging the source and droping it to Target
+            DD.ClickAndHold(Source).MoveToElement(Target).Perform();
+
+            driver.Close();
+
+
+
+
+        }
+    }
+}
--- Selenium-Webdriver/HandlingScrolls.cs
+++ Selenium-Webdriver/HandlingScrolls.cs
@@ -17,14 +17,16 @@ namespace Selenium_Webdriver
             driver.Navigate().GoToUrl("https://testautomationpractice.blogspot.com/");
             driver.Manage().Window.Maximize();
 
+            // Scroll down by 500 pixels
             IJavaScriptExecutor JS = (IJavaScriptExecutor)driver;
             JS.ExecuteScript("window.scrollBy(0, 500);");
 
+            // Scroll down to the bottom
             JS.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
-
 
             IWebElement element = driver.FindElement(By.XPath("//h2[normalize-space()='Footer Links']"));
 
+            // Scroll to view the element
             JS.ExecuteScript("arguments[0].scrollIntoView(true);", element);
 
             driver.Close();
